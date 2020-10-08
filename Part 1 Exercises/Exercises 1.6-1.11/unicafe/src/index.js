79b117a1c0208fb4914226fb97908181456de9d6
camknopp <camknopp@gmail.com>
2020-10-08 16:25:22 -0400

    Completed exercise 1.7

--- Part 1 Exercises/Exercises 1.6-1.11/unicafe/src/index.js
+++ Part 1 Exercises/Exercises 1.6-1.11/unicafe/src/index.js
@@ -7,26 +7,60 @@ const DisplayItem = (props) => (
   </div>
 )
 
-
+const DisplayPercent = (props) => (
+  <div>
+    {props.name} {(props.number)} %
+  </div>
+)
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const increaseGood = () => {
+    setGood(good+1)
+    setAll(all+1)
+    let pos = ((good+1) / (all+1))*100
+    let ave = ((good+1) + (bad*-1)) / (all+1)
+    setPositive(pos)
+    setAverage(ave)
+  }
+
+  const increaseBad = () => {
+    setBad(bad+1)
+    setAll(all+1)
+    let pos = (good / (all+1))*100
+    let ave = (good + ((bad+1)*-1)) / (all+1)
+    setPositive(pos)
+    setAverage(ave)
+  }
+
+  const increaseNeutral = () => {
+    setNeutral(neutral+1)
+    setAll(all+1)
+    let pos = (good / (all+1))*100
+    let ave = (good + (bad*-1)) / (all+1)
+    setPositive(pos)
+    setAverage(ave)
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good+1)}>
+      <button onClick={() => increaseGood()}>
         good
       </button>
 
-      <button onClick={() => setNeutral(neutral+1)}>
+      <button onClick={() => increaseNeutral()}>
         neutral
       </button>
 
-      <button onClick={() => setBad(bad+1)}>
+      <button onClick={() => increaseBad()}>
         bad
       </button>
 
@@ -34,6 +68,10 @@ const App = () => {
       <DisplayItem name={"good"} number={good} />
       <DisplayItem name={"neutral"} number={neutral} />
       <DisplayItem name={"bad"} number={bad} />
+      <DisplayItem name={"all"} number={all} />
+      <DisplayItem name={"average"} number={average} />
+      <DisplayPercent name={"positive"} number={positive} />
+
     </div>
   )
 }
