5a8ed8c58575ac71622fabe279e359833d98e2d1
camknopp <camknopp@gmail.com>
2020-10-09 12:45:49 -0400

    Completed 2.9

--- Part 2 Exercises/Exercises 2.6-2.10/phonebook/src/App.js
+++ Part 2 Exercises/Exercises 2.6-2.10/phonebook/src/App.js
@@ -2,10 +2,13 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    {}
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter ] = useState('')
 
 
   const handleNameChange = (event) => {
@@ -14,6 +17,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
   }
 
   const handleSubmit = (event) => {
@@ -31,9 +38,27 @@ const App = () => {
     }
   }
 
+  const displayFilteredItems = () => {
+    let n = []
+    
+    for (let i = 0; i < persons.length; i++) {
+      if ((persons[i].name.toLowerCase()).includes(newFilter.toLowerCase())) {
+          n.push(persons[i])
+      }
+    }
+  
+    return (
+      n.map(person => <div key={person.name}>{person.name} {person.number}</div>)
+    )
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={newFilter} onChange={handleFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -46,7 +71,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+      <div>{displayFilteredItems()}</div>
+      {/* {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)} */}
     </div>
   )
 }
