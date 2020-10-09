438fe0f3c58ab0677845f42c48bc762be196c11e
camknopp <camknopp@gmail.com>
2020-10-08 21:16:10 -0400

    Completed Exercises 2.2 and 2.3

--- Part 2 Exercises/Exercises 2.1/course-info/src/index.js
+++ Part 2 Exercises/Exercises 2.1/course-info/src/index.js
@@ -1,51 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.part} {props.exercises}</p>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  let p = props.parts
-  return (
-    <div>
-      <Part part={p[0].name} exercises={p[0].exercises} />
-      <Part part={p[1].name} exercises={p[1].exercises} />
-      <Part part={p[2].name} exercises={p[2].exercises} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  let p = props.parts
-  let tot = p[0].exercises + p[1].exercises + p[2].exercises
-  return (
-    <div>
-      <p>Number of exercises {tot}</p>
-    </div>
-  )
-}
-
 const Course = (props) => {
   const { course } = props
   const { parts } = course
+  let sum = parts.reduce((a, b) => {
+    return a + b.exercises
+  }, 0)
+
   return (
     <div>
       <h1>{course.name}</h1>
-      {parts.map(part => <p key={part.id}>
-      {part.name} {part.exercises}</p>)}
+      {parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
+      <b>total of {sum} exercises</b>
     </div>
   )
 }
