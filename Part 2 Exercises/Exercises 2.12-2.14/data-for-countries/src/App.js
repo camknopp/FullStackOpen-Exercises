c20ef09db265be68e294f3f75a697875f1406ba4
camknopp <camknopp@gmail.com>
2020-10-11 11:04:26 -0400

    Working on Exercise 2.14

--- Part 2 Exercises/Exercises 2.12-2.14/data-for-countries/src/App.js
+++ Part 2 Exercises/Exercises 2.12-2.14/data-for-countries/src/App.js
@@ -7,6 +7,7 @@ const App = () => {
   const [ countries, setCountries ] = useState([]) 
   const [ newFilter, setNewFilter ] = useState('')
 
+
   useEffect(() => {
     axios.get('https://restcountries.eu/rest/v2/all')
     .then(response => {
--- Part 2 Exercises/Exercises 2.12-2.14/data-for-countries/src/components/DisplayFilteredItems.js
+++ Part 2 Exercises/Exercises 2.12-2.14/data-for-countries/src/components/DisplayFilteredItems.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
   
 const DisplayFilteredItems = ({countries, filter}) => {
     let n = []
@@ -17,34 +18,64 @@ const DisplayFilteredItems = ({countries, filter}) => {
         )
     } else if (n.length > 1) {
         return (
-            n.map(country => <div key={country.name}>{country.name}</div>)
+            n.map(country => <div key={country.name}>
+                
+                {country.name}
+                
+                </div>)
             )
     } else if (n.length === 1) {
-        console.log(n)
         let country = n[0]
+        let capital = ""
         let languages = country.languages
-        let capital = country.capital
-        if (country.capital == '') {
+        let weather
+        let imageAltText = "flag of " + country.name
+        let ACCESS_KEY = process.env.REACT_APP_API_KEY
+        console.log(ACCESS_KEY)
+
+        if (country.capital === '') {
             capital = 'DNE'
+            axios.get('https://api.weatherstack.com/current', {
+                access_key: ACCESS_KEY,
+                query: country.name
+            })
+            .then(response => {
+            weather = response.data
+            console.log(weather)
+            })
+        } else {
+            capital = country.capital
+            axios.get('https://api.weatherstack.com/current', {
+                access_key: ACCESS_KEY,
+                query: country.capital
+            })
+            .then(response => {
+            weather = response
+            console.log(weather)
+            })
         }
 
         return (
             <div>
                 <h2>{country.name}</h2>
+                <p>Capital: {capital}</p>
+                <p>Population: {country.population}</p>
 
-                <p>capital {capital}</p>
-                <p>population {country.population}</p>
-
-                <h3>languages</h3>
+                <h3>Spoken languages</h3>
                 <ul>
-                    {languages.map((l, i)=> <li>{l.name}</li>)}
+                    {languages.map((l, i)=> <li key={l.name}>{l.name}</li>)}
                 </ul>
-                <img src={country.flag} width="150" height="150"></img>
+
+                <img alt={imageAltText} src={country.flag} width="150" height="150"></img>
+                
+                <h3>Weather in {capital === "DNE" ? country.name : capital}</h3>
+                <p>Temperature: </p>
+                <p>Wind: </p>
             </div>
         )
     } else {
         return (
-            <div></div>
+            <div>No matching countries, specify another filter</div>
         )
     }
 
