e7b4d74de6753fd18ef2789fcc0f215d5291f50a
camknopp <camknopp@gmail.com>
2020-10-13 10:17:04 -0400

    Completed exercise 3.3

--- Part 3 Exercises/Exercises 3.1-3.6/phonebook/index.js
+++ Part 3 Exercises/Exercises 3.1-3.6/phonebook/index.js
@@ -34,6 +34,17 @@ app.get('/info', (request, response) => {
     response.send(msg)
 })
 
+app.get('/api/entries/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const entry = entries.find(entry => entry.id === id)
+
+    if (entry) {
+        response.json(entry)
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
