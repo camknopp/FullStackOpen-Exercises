089f9295540932a522a0313024ec61aa9ce326c7
camknopp <camknopp@gmail.com>
2020-10-14 11:10:46 -0400

    Removed __v from HTTP GET call

--- Part 3 Exercises/Exercises 3.1-3.6/phonebook-backend/models/entry.js
+++ Part 3 Exercises/Exercises 3.1-3.6/phonebook-backend/models/entry.js
@@ -22,4 +22,12 @@ const entrySchema = new mongoose.Schema({
 	number: Number
 })
 
+entrySchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
+})
+
 module.exports = mongoose.model("Entry", entrySchema)
