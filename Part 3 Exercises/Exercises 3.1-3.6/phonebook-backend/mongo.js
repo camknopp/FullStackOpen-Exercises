fcac080f6ee0c0f53248f3d208bab28c7d67bc1a
camknopp <camknopp@gmail.com>
2020-10-14 09:52:20 -0400

    Completed Exercise 3.12

--- Part 3 Exercises/Exercises 3.1-3.6/phonebook-backend/mongo.js
+++ Part 3 Exercises/Exercises 3.1-3.6/phonebook-backend/mongo.js
@@ -1,33 +1,42 @@
-import 'index.js'
-
-const mongoose = require('mongoose')
-
-if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
-}
+const mongoose = require("mongoose")
 
 const password = process.argv[2]
 
-const url =
-  `mongodb+srv://camlk10:${password}@cluster0.khj58.mongodb.net/entries?retryWrites=true&w=majority`
+const url = `mongodb+srv://camlk10:${password}@cluster0.khj58.mongodb.net/entries?retryWrites=true&w=majority`
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(url, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true
+})
 
 const entrySchema = new mongoose.Schema({
-  name: String,
-  number: Number
+	name: String,
+	number: Number
 })
 
-const Entry = mongoose.model('Entry', entrySchema)
+const Entry = mongoose.model("Entry", entrySchema)
 
-const entry = new Entry({
-  content: 'HTML is Easy',
-  date: new Date(),
-  important: true,
-})
+if (process.argv.length < 4) {
+	console.log("phonebook:")
+	Entry.find({}).then(result => {
+		result.forEach(entry => {
+			console.log(`${entry.name} ${entry.number}`)
+		})
+		mongoose.connection.close()
+	})
+} else {
+	const name = process.argv[3]
+	const number = process.argv[4]
 
-note.save().then(result => {
-    console.log('note saved!')
-    mongoose.connection.close()
-  })
+	const entry = new Entry({
+		name: name,
+		number: number
+	})
+
+	entry.save().then(result => {
+		console.log(`Added ${name} number ${number} to phonebook`)
+		mongoose.connection.close()
+	})
+}
