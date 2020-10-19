b2a08310763a5e7b97bcc0b5897c7c23f1f871db
camknopp <camknopp@gmail.com>
2020-10-19 13:38:26 -0400

    Completed exercise 4.4

--- Part 4 Exercises/bloglist/index.js
+++ Part 4 Exercises/bloglist/index.js
@@ -1,6 +1,6 @@
 const config = require("./utils/config")
-const logger = require('./utils/logger')
-const app = require('./App')
+const logger = require("./utils/logger")
+const app = require("./App")
 
 app.listen(config.PORT, () => {
 	logger.info(`Server running on port ${config.PORT}`)
--- Part 4 Exercises/bloglist/utils/list_helper.js
+++ Part 4 Exercises/bloglist/utils/list_helper.js
@@ -1,7 +1,14 @@
-const dummy = (blogs) => {
-    return 1
+const dummy = blogs => {
+	return 1
+}
+
+const totalLikes = blogs => {
+	return blogs.length === 0 ? 0 : blogs.reduce((sum, item) => {
+		return sum + item.likes
+	}, 0)
 }
 
 module.exports = {
-    dummy
+	dummy,
+	totalLikes
 }
