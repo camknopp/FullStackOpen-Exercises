95dd41d9a1fccefd9e30151607422592157a558f
camknopp <camknopp@gmail.com>
2020-10-19 14:05:32 -0400

    Completed exercise 4.5

--- Part 4 Exercises/bloglist/utils/list_helper.js
+++ Part 4 Exercises/bloglist/utils/list_helper.js
@@ -8,7 +8,21 @@ const totalLikes = blogs => {
 	}, 0)
 }
 
+const favoriteBlog = blogs =>{
+    maxLikes = 0
+    favorite = null
+
+    for(let i = 0; i < blogs.length; i++) {
+        if (blogs[i].likes > maxLikes) {
+            maxLikes = blogs[i].likes
+            favorite = blogs[i]
+        }
+    }
+    return favorite
+}
+
 module.exports = {
 	dummy,
-	totalLikes
+    totalLikes,
+    favoriteBlog
 }
