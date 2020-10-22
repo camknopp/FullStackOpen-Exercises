f0c29ce822d33a208ef41f0e8003514e378dad7e
camknopp <camknopp@gmail.com>
2020-10-22 11:40:04 -0400

    Completed exercise 4.16

--- Part 4 Exercises/bloglist/controllers/users.js
+++ Part 4 Exercises/bloglist/controllers/users.js
@@ -10,6 +10,14 @@ usersRouter.get("/", async (request, response) => {
 usersRouter.post('/', async (request, response) => {
     const body = request.body
     
+    if (body.password.length < 3) {
+        return response.status(400).json({error: "password must be longer than 3 characters"})
+    }
+
+    if (body.username.length < 3) {
+        return response.status(400).json({error: "username must be longer than 3 characters"})
+    }
+
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
