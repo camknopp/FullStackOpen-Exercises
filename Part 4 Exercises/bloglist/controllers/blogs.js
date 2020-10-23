8ae84ef2ba3511df5cd2924bedba2b2330fcef8b
camknopp <camknopp@gmail.com>
2020-10-23 10:38:48 -0400

    Completed exercise 4.20
    Refactored the token extractor into middleware

--- Part 4 Exercises/bloglist/controllers/blogs.js
+++ Part 4 Exercises/bloglist/controllers/blogs.js
@@ -3,25 +3,12 @@ const Blog = require("../models/blog")
 const User = require("../models/user")
 const jwt = require("jsonwebtoken")
 
-const getTokenFrom = request => {
-	const authorization = request.get("authorization")
-	if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-		return authorization.substring(7)
-	}
-	return null
-}
-
-blogsRouter.get("/", async (request, response) => {
-	const blogs = await Blog.find({}).populate("user", { blogs: 0 })
-	response.json(blogs)
-})
-
 blogsRouter.post("/", async (request, response) => {
 	const body = request.body
 
 	const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
-	if (!token || !decodedToken.id) {
+	if (!request.token || !decodedToken.id) {
 		return response.status(401).json({ error: "token missing or invalid" })
 	}
 
--- Part 4 Exercises/bloglist/utils/middleware.js
+++ Part 4 Exercises/bloglist/utils/middleware.js
@@ -1,9 +1,13 @@
 const logger = require("./logger")
 
 const tokenExtractor = (request, response, next) => {
-	const authorization = request.get("authorization")
+  const authorization = request.get("authorization")
+
+  logger.info(authorization)
 	if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    logger.info("setting token")
     request.token = authorization.substring(7)
+    logger.info("request.token: ", request.token)
 	} else {
 		request.token = null
 	}
