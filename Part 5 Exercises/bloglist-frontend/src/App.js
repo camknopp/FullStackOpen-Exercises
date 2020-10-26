9b03c3166dbf84b8e8f5d4fe1f0782334876a310
camknopp <camknopp@gmail.com>
2020-10-26 11:19:50 -0400

    Completed exercise 5.4

--- Part 5 Exercises/bloglist-frontend/src/App.js
+++ Part 5 Exercises/bloglist-frontend/src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react"
 import Blog from "./components/Blog"
 import blogService from "./services/blogs"
 import loginService from "./services/login"
+import ErrorMessage from "./components/errorMessage"
+import Notification from "./components/notification"
 
 const App = () => {
 	const [blogs, setBlogs] = useState([])
@@ -11,6 +13,8 @@ const App = () => {
 	const [title, setTitle] = useState("")
 	const [author, setAuthor] = useState("")
 	const [url, setUrl] = useState("")
+	const [notificationMessage, setNotificationMessage] = useState(null)
+	const [errorMessage, setErrorMessage] = useState(null)
 
 	useEffect(() => {
 		blogService.getAll().then(blogs => setBlogs(blogs))
@@ -35,50 +39,72 @@ const App = () => {
 				password
 			})
 
-			window.localStorage.setItem("loggedInUser", JSON.stringify(user))
+      window.localStorage.setItem("loggedInUser", JSON.stringify(user))
+      
+      setNotificationMessage(`Welcome back ${user.username}`)
+			setTimeout(() => {
+				setNotificationMessage(null)
+			}, 5000)
 
-      //blogService.setToken(user.token)
+			//blogService.setToken(user.token)
 			setUser(user)
 			setUsername("")
 			setPassword("")
 		} catch {
 			// setErrorMessage()
-      console.log("error: wrong credentials")
-      setUsername("")
+			setErrorMessage("invalid credentials")
+			setTimeout(() => {
+				setErrorMessage(null)
+			}, 5000)
+			setUsername("")
       setPassword("")
-		}
+    }
+    
+    
+
 		// console.log("logging in with", username, password)
 	}
 
 	const handleLogout = event => {
-    // window.localStorage.removeItem("loggedInUser")
-    window.localStorage.clear()
+		// window.localStorage.removeItem("loggedInUser")
+		window.localStorage.clear()
 		setUser(null)
 	}
 
 	const handleCreate = async event => {
 		// need to create a new note
-    event.preventDefault()
-    const loggedUser = window.localStorage.getItem("loggedInUser")
-    const user = JSON.parse(loggedUser)
-    const token = user.token
+		event.preventDefault()
+		const loggedUser = window.localStorage.getItem("loggedInUser")
+		const user = JSON.parse(loggedUser)
+		const token = user.token
 
 		const newBlog = {
 			title,
 			author,
 			url
-    }
-    
-    await blogService.create(newBlog, token)
-    setTitle("")
-    setAuthor("")
+		}
+
+		try {
+      await blogService.create(newBlog, token)
+      setNotificationMessage(`added new blog, ${newBlog.title}`)
+		} catch {
+			setErrorMessage("error creating new blog")
+			setTimeout(() => {
+				setErrorMessage(null)
+			}, 5000)
+		}
+		setTitle("")
+		setAuthor("")
     setUrl("")
+    
 	}
 
 	if (user === null) {
 		return (
 			<div>
 				<h2>Log in to application</h2>
+        <Notification message={notificationMessage} />
+			<ErrorMessage message={errorMessage} />
 				<form onSubmit={handleLogin}>
 					<div>
 						username
@@ -105,6 +131,8 @@ const App = () => {
 	return (
 		<div>
 			<h2>blogs</h2>
+			<Notification message={notificationMessage} />
+			<ErrorMessage message={errorMessage} />
 			<div>
 				{user.name} is logged in
 				<button type="submit" onClick={handleLogout}>
@@ -133,8 +161,8 @@ const App = () => {
 					value={url}
 					onChange={({ target }) => setUrl(target.value)}
 				></input>
-        <br></br>
-        <button type="submit">create</button>
+				<br></br>
+				<button type="submit">create</button>
 			</form>
 
 			<br></br>
