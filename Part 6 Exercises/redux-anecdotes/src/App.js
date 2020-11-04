848a2a198a6d8234bcc5a552a3a8a45f0e6f7b98
camknopp <camknopp@gmail.com>
2020-11-04 09:47:30 -0500

    Completed exercise 6.5
    Anecdotes are now ordered in ascending order based on the number of votes they have

--- Part 6 Exercises/redux-anecdotes/src/App.js
+++ Part 6 Exercises/redux-anecdotes/src/App.js
@@ -4,18 +4,28 @@ import { vote, addAnecdote } from "./reducers/anecdoteReducer"
 
 const App = () => {
 	const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-  
-  const newAnecdote = (event) => {
-    event.preventDefault()
-    let content = event.target.content.value
-    dispatch(addAnecdote(content))
-  }
+	const dispatch = useDispatch()
+
+	const newAnecdote = event => {
+		event.preventDefault()
+		let content = event.target.content.value
+		dispatch(addAnecdote(content))
+	}
+
+	const compare = (a, b) => {
+		if (a.votes > b.votes) {
+			return -1
+		} else if (a.votes < b.votes) {
+			return 1
+		} else {
+			return 0
+		}
+	}
 
 	return (
 		<div>
 			<h2>Anecdotes</h2>
-			{anecdotes.map(anecdote => (
+			{anecdotes.sort(compare).map(anecdote => (
 				<div key={anecdote.id}>
 					<div>{anecdote.content}</div>
 					<div>
@@ -27,7 +37,7 @@ const App = () => {
 			<h2>create new</h2>
 			<form onSubmit={newAnecdote}>
 				<div>
-					<input name='content' />
+					<input name="content" />
 				</div>
 				<button type="submit">create</button>
 			</form>
