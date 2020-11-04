7ab53d232f17c590107949aa57db6cd0a081d78d
camknopp <camknopp@gmail.com>
2020-11-04 09:54:15 -0500

    Completed exercises 6.6 and 6.7
    Separated action-creator functoins into reducer file and separated new anecdote creation mechanisms into their own component

--- Part 6 Exercises/redux-anecdotes/src/App.js
+++ Part 6 Exercises/redux-anecdotes/src/App.js
@@ -1,16 +1,11 @@
 import React from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { vote, addAnecdote } from "./reducers/anecdoteReducer"
+import AnecdoteForm from "./components/AnecdoteForm"
 
 const App = () => {
 	const anecdotes = useSelector(state => state)
 	const dispatch = useDispatch()
-
-	const newAnecdote = event => {
-		event.preventDefault()
-		let content = event.target.content.value
-		dispatch(addAnecdote(content))
-	}
 
 	const compare = (a, b) => {
 		if (a.votes > b.votes) {
@@ -34,13 +29,7 @@ const App = () => {
 					</div>
 				</div>
 			))}
-			<h2>create new</h2>
-			<form onSubmit={newAnecdote}>
-				<div>
-					<input name="content" />
-				</div>
-				<button type="submit">create</button>
-			</form>
+      <AnecdoteForm />
 		</div>
 	)
 }
