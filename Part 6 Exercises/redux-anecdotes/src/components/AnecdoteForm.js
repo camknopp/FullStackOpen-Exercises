72f7a19f007d1b22641440cf130fff7fff7ff9e3
camknopp <camknopp@gmail.com>
2020-11-05 09:13:32 -0500

    Completed exercise 6.14

--- Part 6 Exercises/redux-anecdotes/src/components/AnecdoteForm.js
+++ Part 6 Exercises/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,14 +1,16 @@
 import React from "react"
 import { useDispatch } from "react-redux"
 import { addAnecdote } from "../reducers/anecdoteReducer"
+import anecdoteService from "../services/anecdotes"
 
 const AnecdoteForm = () => {
 
 	const dispatch = useDispatch()
 
-	const newAnecdote = event => {
+	const newAnecdote = async event => {
 		event.preventDefault()
 		let content = event.target.content.value
+		const newAnec = await anecdoteService.createNew(content)
 		dispatch(addAnecdote(content))
 	}
 
--- Part 6 Exercises/redux-anecdotes/src/services/anecdotes.js
+++ Part 6 Exercises/redux-anecdotes/src/services/anecdotes.js
@@ -2,9 +2,22 @@ import axios from 'axios'
 
 const baseurl = "http://localhost:3001/anecdotes"
 
+const getId = () => (100000 * Math.random()).toFixed(0)
+
 const getAll = async () => {
     const response = await axios.get(baseurl)
     return response.data
 }
 
-export default { getAll }
+const createNew = async (content) => {
+    const newAnec = {
+        content: content,
+        id: getId(),
+        votes: 0
+    }
+
+    const response = await axios.post(baseurl, newAnec)
+    return response.data
+}
+
+export default { getAll, createNew }
